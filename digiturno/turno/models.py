a04338a44500866d30897aeb871eae1fa2c94c60
diego <puertorojas@gmail.com>
2017-02-22 18:08:03 -0500

    Se crea la funcionalidad para la asignacion de turnos por parte del usuario recepcion

--- digiturno/turno/models.py
+++ digiturno/turno/models.py
@@ -3,7 +3,7 @@ from account.models import Usuario
 from django.db import models
 
 class Turno(models.Model):
-    turno = models.CharField(null=True, max_length=5)
+    turno = models.CharField(null=True, max_length=5, default=5)
     recepcionista = models.ForeignKey(Usuario,
                                       verbose_name="Persona que asigno la cita",
                                       related_name="recepcion_turnos")
@@ -14,8 +14,8 @@ class Turno(models.Model):
                                verbose_name="Asesor que atiende al Usuario en este turno", 
                                related_name="asesor_turnos", null=True)
     fecha_ingreso = models.DateTimeField(auto_now_add=True, null=True)
-    fecha_inicio = models.DateTimeField()
-    fecha_fin = models.DateTimeField()
+    fecha_inicio = models.DateTimeField(null=True)
+    fecha_fin = models.DateTimeField(null=True)
 
     def __str__(self):
         return "No. %r" % (self.turno)
@@ -25,4 +25,5 @@ class Turno(models.Model):
                 return True
             else:
                 return False
+
 
