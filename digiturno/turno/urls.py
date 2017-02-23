6fd426f3b6b807333a2c5c64a94f76cfb37e07d7
Juan Mejia <jgmejia@gmail.com>
2017-02-22 23:07:28 -0500

    Insercion de URLs para vistas cambiar y cola

--- digiturno/turno/urls.py
+++ digiturno/turno/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^asignar/(?P<usuario_id>\d+)?/$', views.asignar, name='asignar'),
+    url(r'^cola/$', views.cola, name='cola'),
+    url(r'^cambiar/$', views.cambiar, name='cambiar'),
 ]
