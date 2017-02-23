1ab8c6a9cdf1085408a36a8a350890fb71722103
Juan Mejia <jgmejia@gmail.com>
2017-02-22 23:08:33 -0500

    Creacion de vistas cola y cambiar

--- digiturno/turno/views.py
+++ digiturno/turno/views.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 @login_required
 def asignar(request, usuario_id ):
-        return HttpResponse('Este es el usuario al que le vamos a asignar el turno:%r' % (usuario_id))
+    return HttpResponse('Este es el usuario al que le vamos a asignar el turno:%r' % (usuario_id))
+
+def cola(request):
+    return render(request,'turno/cola.html')
+
+@login_required
+def cambiar(request):
+    user = request.user
+    return render(request,'turno/cambiar.html', {'user':user})
